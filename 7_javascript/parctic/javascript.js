f9a0d3b310a7ae49946cea0e8eeee921d2284a6c
priyanshirabadiya <139095730+priyanshirabadiya@users.noreply.github.com>
2024-01-26 06:22:48 +0530

    26/1/23

---
+++ 7_javascript/parctic/javascript.js
@@ -0,0 +1,25 @@
+// function Language() {
+// 	let first = 'HTML',
+// 		second = 'CSS',
+// 		Third = 'Javascript'
+// 	return {
+// 		first,
+// 		second,
+// 		Third
+// 	};
+// }
+// let { first, second, Third } = Language();
+// console.log(first);
+// console.log(second);
+// console.log(Third);
+
+console.log('Boolean(10) is ' + Boolean(10));
+console.log('Boolean("GeeksforGeeks") is '
+			+ Boolean("GeeksforGeeks"));
+console.log('Boolean(2.74) is ' + Boolean(2.74));
+console.log('Boolean(-1) is ' + Boolean(-1));
+console.log("Boolean('true') is " + Boolean('true'));
+console.log("Boolean('false') is " + Boolean('false'));
+console.log('Boolean(3 * 2 + 1.11) is '
+			+ Boolean(3 * 2 + 1.11));
+console.log('Boolean(1>2) is ' + Boolean(1 > 2));
